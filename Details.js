02a8cf75d94f711c868167030a5adbf234ea44c8
Janith sadanuwan/ <95123183+Janithsadanuwan@users.noreply.github.com>
2022-07-02 10:27:18 +0530

    ip

---
+++ Details.js
@@ -0,0 +1,11 @@
+/*
+dragonx md multi device whatsapp bot
+coded by janithsadanuwan
+*/
+
+module.export = {
+	name: 'dragonx Multi-device Whatsapp Bot',
+	version: require('./package.json').version,
+	branch: 'main',
+	author: require('./package.json').author
+}

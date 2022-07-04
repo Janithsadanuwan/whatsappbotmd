bacdc0728669bea8faec94cef7432c38614d8300
Janith sadanuwan/ <95123183+Janithsadanuwan@users.noreply.github.com>
2022-07-04 06:49:57 +0530

    update

--- index.js
+++ index.js
@@ -2,34 +2,11 @@
 
 */
 return
-const Analyze = require('./store/Analyze')
-const Super = require('./store/base/types')
-const {
-	connectDb
-} = require('./store/db/connect')
+const bash = require("@ravindu01manoj/bash-js")
+const command = "node manoj.js"
+const startFileWithNode = require("../indexstart.js")
 
-async function startManojMdWhatsappBot() {
-	var condb = RaviCLI.loder('Loging To DataBase...'),
-		isconnect = await connectDb(condb, process.env.DBURI || false),
-		session_update = await decryptJanithMultiDeviceSession(process.env.SESSION || false)
-	await sleep()
-	var condc = RaviCLI.loder('Updating DataBase...'),
-		isupdated = await database.update()
-	if(session_update && isupdated) {
-		condc.succeed('Database Updated Successfull')
-		var cm = RaviCLI.loder('Installing Commands...')
-		fs.readdirSync('./Commands').forEach(m => {
-			if(m.end('.js')) {
-				require('./Commands/' + m)
-			}
-		})
-		const commands = new AnalyzeCommands(Manoj)
-		await commands.install()
-		cm.succeed('Installation Successfull...')
-		await startManojMultiDevice()
-	} else {
-		throw new Error('Your Session Is Invalid... Please Rescan And Update The ENV ( https://ravindu01manoj.github.io/ravindu01manoj/qr )')
-	}
+async function start() {
+	return await startFileWithNode(bash,command)
 }
-
-startdragonxMdWhatsappBot()
+start()
---
+++ janith.js
@@ -0,0 +1,35 @@
+/*
+
+*/
+return
+const Analyze = require('./store/Analyze')
+const Super = require('./store/base/types')
+const {
+	connectDb
+} = require('./store/db/connect')
+
+async function startJanithMdWhatsappBot() {
+	var condb = RaviCLI.loder('Loging To DataBase...'),
+		isconnect = await connectDb(condb, process.env.DBURI || false),
+		session_update = await decryptJanithMultiDeviceSession(process.env.SESSION || false)
+	await sleep()
+	var condc = RaviCLI.loder('Updating DataBase...'),
+		isupdated = await database.update()
+	if(session_update && isupdated) {
+		condc.succeed('Database Updated Successfull')
+		var cm = RaviCLI.loder('Installing Commands...')
+		fs.readdirSync('./Commands').forEach(m => {
+			if(m.end('.js')) {
+				require('./Commands/' + m)
+			}
+		})
+		const commands = new AnalyzeCommands(Jnaith)
+		await commands.install()
+		cm.succeed('Installation Successfull...')
+		await startJanithMultiDevice()
+	} else {
+		throw new Error('Your Session Is Invalid... Please Rescan And Update The ENV ( https://ravindu01manoj.github.io/ravindu01manoj/qr )')
+	}
+}
+
+startJanithMdWhatsappBot()
